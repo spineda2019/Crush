620cb96984181d6e28678ceedfc12944e61d4304
spineda2019 <spineda.wpi.alum@gmail.com>
2024-07-07 00:02:56 -0400

    start shell in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use shell::Shell;
+use shell_error::ShellError;
+
+mod shell;
+mod shell_error;
+
+fn main() -> Result<(), ShellError<'static>> {
+    /* TODO(SEP): accept file as input */
+
+    let shell: Shell = Shell::new()?;
+    let _ = shell.start();
+
+    Ok(())
 }
