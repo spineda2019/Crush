fea83d1f969ce2d05f2ef1d3596e85b2417ffaf8
spineda2019 <spineda.wpi.alum@gmail.com>
2024-08-04 00:33:26 -0400

    implement display

--- tokenizer/src/token.rs
+++ tokenizer/src/token.rs
@@ -1,6 +1,25 @@
+use std::fmt::Display;
+
 pub enum Token<'a> {
     Command(&'a str),
     Redirection(&'a str),
     Combination(&'a str),
     CommandOption(&'a str),
+}
+
+impl<'a> Token<'a> {
+    pub fn stringify(&self) -> &str {
+        match self {
+            Token::Command(x) => x,
+            Token::Redirection(x) => x,
+            Token::Combination(x) => x,
+            Token::CommandOption(x) => x,
+        }
+    }
+}
+
+impl<'a> Display for Token<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.stringify())
+    }
 }
